ffcf58b5a68364d6f3a853fd570f3472cc6587ef
Hamza Rahim <hamzarahim836@gmail.com>
2025-10-14 18:40:03 +0500

    first commit

--- frontend/src/components/table/index.ts
+++ frontend/src/components/table/index.ts
@@ -1,4 +1,17 @@
-export * from "./DataTable";
-export * from "./useUrlTableState";
+export { DataTable } from "./DataTable";
+export type {
+    DataTableProps,
+    TableColumn,
+    RowAction,
+    SortDirection as TableSortDirection,
+} from "./DataTable";
+
+export { useUrlTableState } from "./useUrlTableState";
+export type {
+    TableUrlState,
+    UseUrlTableStateOptions,
+    UseUrlTableStateResult,
+    SortDirection as UrlSortDirection,
+} from "./useUrlTableState";
 
 
